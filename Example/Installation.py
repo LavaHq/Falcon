71531eac127cac3671690f64531e4a8a8632cbc1
DanielHabib <d.g.habib7@gmail.com>
2016-01-27 11:19:00 -0500

    Get some of the Vehicles Stats

--- Example/Installation.py
+++ Example/Installation.py
@@ -1,7 +1,21 @@
-print "Start Simulator (SITL)"
+from dronekit import connect, VehicleMode
+
 from dronekit_sitl import SITL
 
-sitl = SITL()
-sitl.download('copter', '3.3', verbose=True)
-sitl_args = ['-IO', '--model', 'quad', '--home=-35.363261,149.165230,584,353']
-sitl.launch(sitl_args, await_ready=True, restart=True)
+if __name__ == '__main__':
+    print "Start Simulator (SITL)"
+
+    sitl = SITL()
+    sitl.download('copter', '3.3', verbose=True)
+    sitl_args = ['-IO', '--model', 'quad', '--home=-35.363261,149.165230,584,353']
+    sitl.launch(sitl_args, await_ready=True, restart=True)
+
+    # Connect to the Vehicle
+    vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
+
+    # Get Vehicle Attributes
+    print "Simultaor Statistics"
+    print " GPS: {0}".format(vehicle.gps_0)
+    print " Battery: {0}".format(vehicle.battery)
+    print " Last Heartbeat: {0}".format(vehicle.hear)
+
