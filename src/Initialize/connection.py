25303810179f4e0d3c45be8965844d1d702f6b54
DanielHabib <d.g.habib7@gmail.com>
2016-02-09 10:04:52 -0500

    Create Connection Class

---
+++ src/Initialize/connection.py
@@ -0,0 +1,34 @@
+import socket
+import exceptions
+
+from dronekit import connect, APIException
+
+
+class FalconApp:
+    """The Main Drone Connection Module
+
+    Attributes:
+        vehicle (Vehicle): the vehicle
+    """
+    def __init__(self, ip, heartbeat_timeout=10):
+        """
+        Args:
+            ip (string): The IP Address that builds the connection
+            heartbeat_timeout (int): Time in seconds of the Timeout
+
+        """
+        try:
+            self.vehicle = connect(ip, wait_ready=True, heartbeat_timeout=heartbeat_timeout)
+        # Bad TCP connection
+        except socket.error:
+            print 'No server exists!'
+
+        # Bad TTY connection
+        except exceptions.OSError as e:
+            print 'No serial exists! error:{0}'.format(e)
+
+        # Timeout Error
+        except APIException:
+            print 'API Exception Thrown'
+        except Exception, e:
+            print 'General Exception: {0}'.format(e)
---
+++ test/initialize/connection_test.py
@@ -0,0 +1,7 @@
+from unittest import TestCase
+
+
+class ConnectionTest(TestCase):
+
+    def test_connection(self):
+        pass
