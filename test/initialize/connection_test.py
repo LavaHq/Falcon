361dfc9c462128a3f792380fc309806880d64767
DanielHabib <d.g.habib7@gmail.com>
2016-02-09 18:31:46 -0500

    Update Connection Test

--- test/initialize/connection_test.py
+++ test/initialize/connection_test.py
@@ -1,6 +1,5 @@
 from dronekit import Vehicle
-from faker import Faker
-from mock import Mock, patch
+from mock import patch
 from unittest import TestCase
 
 from src.initialize.connection import FalconConnection
@@ -8,13 +7,9 @@ from src.initialize.connection import FalconConnection
 
 class FalconConnectionTest(TestCase):
 
-    def setUp(self):
-        self.fake = Faker()
-
     @patch('dronekit.connect')
     def test_initialize_with_ip(self, dronekit_patch):
         ip = 'ip'
-        falconConnection = FalconConnection(ip)
-        isinstance(Vehicle, type(falconConnection.vehicle))
+        falcon_connection = FalconConnection(ip)
+        isinstance(Vehicle, type(falcon_connection.vehicle))
         dronekit_patch.assert_called_with(ip, heartbeat_timeout=10, wait_ready=True)
-
